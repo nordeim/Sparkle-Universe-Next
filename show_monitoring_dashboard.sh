f681f0897357e1c18e90bc08f48ebbf46284eb54
nordeim <nordeimkuahsziyp@outlook.com>
2025-08-09 06:02:17 +0800

    Create show_monitoring_dashboard.sh

---
+++ show_monitoring_dashboard.sh
@@ -0,0 +1,11 @@
+-- Full dashboard
+psql -h localhost -p 5433 -U sparkle_user -d sparkle_db -f prisma/migrations/dashboard.sql
+
+-- Health check
+psql -h localhost -p 5433 -U sparkle_user -d sparkle_db -c "SELECT * FROM check_system_health();"
+
+-- Cache statistics
+psql -h localhost -p 5433 -U sparkle_user -d sparkle_db -c "SELECT * FROM simple_cache_stats;"
+
+-- Find unused indexes
+psql -h localhost -p 5433 -U sparkle_user -d sparkle_db -c "SELECT * FROM find_unused_indexes();"
