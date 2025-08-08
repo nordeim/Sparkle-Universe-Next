2b3a6a85d9127c2804e54b20bcfd17ebc2444223
nordeim <nordeimkuahsziyp@outlook.com>
2025-08-09 06:11:00 +0800

    Update show_monitoring_dashboard.sh

--- show_monitoring_dashboard.sh
+++ show_monitoring_dashboard.sh
@@ -9,3 +9,6 @@ psql -h localhost -p 5433 -U sparkle_user -d sparkle_db -c "SELECT * FROM simple
 
 # Find unused indexes
 psql -h localhost -p 5433 -U sparkle_user -d sparkle_db -c "SELECT * FROM find_unused_indexes();"
+
+# Monitor index usage over time
+psql -h localhost -p 5433 -U sparkle_user -d sparkle_db -f prisma/migrations/index_monitor.sql
